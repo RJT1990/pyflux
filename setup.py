40fb257f72d74a15f7274feeb2571e70dffe3e2b
rjt1990 <rj-taylor@live.co.uk>
2016-03-30 20:25:23 +0100

    PyFlux Release 0.1.5

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'pyflux',
-  packages = ['pyflux','pyflux.arma','pyflux.gas','pyflux.inference','pyflux.output','pyflux.tests'], 
-  version = '0.1.4',
+  packages = ['pyflux','pyflux.arma','pyflux.distributions','pyflux.gas','pyflux.inference','pyflux.output','pyflux.tests','pyflux.var'], 
+  version = '0.1.5',
   description = 'A time-series analysis library for Python',
   author = 'Ross Taylor',
   author_email = 'rj-taylor@live.co.uk',
   url = 'https://github.com/rjt1990/pyflux', 
-  download_url = 'https://github.com/rjt1990/pyflux/tarball/0.1.4', 
+  download_url = 'https://github.com/rjt1990/pyflux/tarball/0.1.5', 
   keywords = ['time series','machine learning','bayesian statistics'],
   license = 'BSD',
   install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'seaborn','numdifftools']
